7a44743ccdabd3f2565a221d03fe275026fda0b1
LIENPHAM-HAMK <lien.pham@student.hamk.fi>
2024-10-16 21:19:58 +0300

    Add method to hint the right name

--- src/App.java
+++ src/App.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static String getNameHint(String name) {
+        if (name != null && name.length() >= 2) {
+            return name.substring(0, 1); // Returns the first two characters
+        } else {
+            return ""; // Return an empty string if the name is null or too short
+        }
+    }
+        public static void main(String[] args) throws Exception {
         String input1;
         String input2;
         input1 = " ";
@@ -9,12 +16,13 @@ public class App {
         name = "Tommy";
         Scanner in = new Scanner(System.in);
         int count = 0; 
-               
+        String hint = getNameHint(name);
         while (true)
         {
             System.out.println("Guess my name, please ? ");
             input1 = in.nextLine();
             count++;
+            System.out.println("Hint: " + hint);
             System.out.println("Do you want to quit? ");
             input2 = in.nextLine(); 
             
@@ -23,16 +31,17 @@ public class App {
             {
                 System.out.println("You guessed " + count + " times.");
                 break; 
-            }           
-
+            } 
             if (input1.equalsIgnoreCase(name))
             {
                 System.out.println("Congratulations! ");
                 System.out.println("You guessed " + count + " times.");
                 break;
-
-            }          
-            
+            }  
+            if (count == 2)
+            {
+                System.out.println("The 2 first letters of the name is TO");
+            }        
             
         }      
         
